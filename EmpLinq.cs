7e5d9aac91ef7d2e973d2492be13ecd60a5cb589
vinay <vinaykumarreddy.g81@gmail.com>
2022-09-01 17:55:24 +0530

    01/09/2022

--- EmpLinq.cs
+++ EmpLinq.cs
@@ -1,42 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BATCH1_DET_2022
-{
-    public class EmpLinq
-    {
+//namespace BATCH1_DET_2022
+//{
+//    public class EmpLinq
+//    {
        
-            public EmpLinq()
-            {
+//            public EmpLinq()
+//            {
 
-            }
+//            }
 
-            public EmpLinq(int id, string name, string dept)
-            {
-                this.ID = id;
-                this.Name = name;
-                this.Dept = dept;
+//            public EmpLinq(int id, string name, string dept)
+//            {
+//                this.ID = id;
+//                this.Name = name;
+//                this.Dept = dept;
 
-            }
-            public int ID { get; set; }
-            public string Name { get; set; }
-            public string Dept { get; set; }
+//            }
+//            public int ID { get; set; }
+//            public string Name { get; set; }
+//            public string Dept { get; set; }
 
-        private static List<Employee> GetListofEmployees()
-        {
-            List<Employee> emps = new List<Employee>();
-            emps.Add(new EmpLinq(1, "John", "Training"));
-            emps.Add(new Employee(2, "Mark", "Finance"));
-            emps.Add(new Employee(3, "Peter", "Technical"));
-            emps.Add(new Employee(4, "Bob", "Technical"));
-            emps.Add(new Employee(5, "Robert", "Finance"));
-            emps.Add(new Employee(6, "Jason", "Training"));
+//        private static List<Employee> GetListofEmployees()
+//        {
+          
 
-            return emps;
-        }
+//            return emps;
+//        }
 
     }
     }
